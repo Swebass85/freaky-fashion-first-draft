7f83be99e92badadd3b21cd20a0533f5533ed7d5
Sebastian <akermans2@gmail.com>
2026-05-08 13:13:08 +0200

    set up database, added products to ejs

--- database/init.js
+++ database/init.js
@@ -4,6 +4,6 @@ const path = require("path");
 const db = new Database(
   path.join(__dirname, "freaky-fashion.db")
 );
-const schema = fs.readFileSync("./schema.sql", "utf-8");
+const schema = fs.readFileSync(path.join(__dirname, "schema.sql"), "utf-8");
 db.exec(schema);
 console.log("✅ Database initialized successfully");
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./database/freaky-fashion.db");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  db.all("SELECT * FROM products", [], (err, products) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.render("index", {
+      title: "Freaky Fashion",
+      products: products,
+    });
+  });
 });
 
 module.exports = router;
