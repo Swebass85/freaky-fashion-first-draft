60a52debf6c205b99af2316cad7dad07b4555de3
Sebastian <akermans2@gmail.com>
2026-05-15 13:08:49 +0200

    added product page and set up db routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,32 @@ const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./database/freaky-fashion.db");
 
+
+router.get("/search", function (req, res, next) {
+  const searchQuery = req.query.q;
+
+  db.all(
+    `
+    SELECT *
+    FROM products
+    WHERE type LIKE ?
+    OR brand LIKE ?
+    `,
+    [`%${searchQuery}%`, `%${searchQuery}%`],
+    (err, products) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.render("search-results", {
+        title: "Search Results",
+        products: products,
+        searchQuery: searchQuery
+      });
+    }
+  );
+});
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   db.all(
@@ -28,27 +54,38 @@ router.get("/", function (req, res, next) {
 router.get("/products/:id", function (req, res, next) {
   const productId = req.params.id;
 
+  // 1. Get the selected product
   db.get(
-    `
-    SELECT *
-    FROM products
-    WHERE id = ?
-    `,
+    `SELECT * FROM products WHERE id = ?`,
     [productId],
     (err, product) => {
-      if (err) {
-        return next(err);
-      }
+      if (err) return next(err);
 
       if (!product) {
         return res.status(404).send("Product not found");
       }
 
-      res.render("product", {
-        title: product.type,
-        product: product,
-      });
-    },
+      // 2. Get other products for carousel
+      db.all(
+        `
+        SELECT *
+        FROM products
+        WHERE id != ?
+        LIMIT 6
+        `,
+        [productId],
+        (err, relatedProducts) => {
+          if (err) return next(err);
+
+          // 3. Render page with BOTH datasets
+          res.render("products", {
+            title: product.type,
+            product: product,
+            relatedProducts: relatedProducts
+          });
+        }
+      );
+    }
   );
 });
 
